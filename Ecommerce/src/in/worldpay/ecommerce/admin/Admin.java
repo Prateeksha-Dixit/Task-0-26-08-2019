57f535a3a6042603acaa0f768a260404d3cea813
Prateeksha Dixit <Prateeksha Dixit@DESKTOP-U8N1HGB>
2019-08-26 21:12:18 +0530

    Task-1-26-08-2019

---
+++ Ecommerce/src/in/worldpay/ecommerce/admin/Admin.java
@@ -0,0 +1,17 @@
+package in.worldpay.ecommerce.admin;
+import in.worldpay.ecommerce.user.*;
+
+public class Admin {
+
+	public static void main(String[] args) {
+		SetName username=new SetName();
+		username.setUserName("PrateekshaDixit");
+		
+		SetPassword password=new SetPassword();
+		password.setPassword("prateeksha123");
+		
+		
+
+	}
+
+}
